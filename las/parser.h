8139ce814f7374186cf1410ad7be5bfe115ebf03
Hirochika Asai <asai@scyphus.co.jp>
2013-11-17 14:14:20 -0700

    Added las/parser.h.

---
+++ las/parser.h
@@ -0,0 +1,37 @@
+/*_
+ * Copyright 2013 Scyphus Solutions Co. Ltd.  All rights reserved.
+ *
+ * Authors:
+ *      Hirochika Asai  <asai@scyphus.co.jp>
+ */
+
+/* $Id$ */
+
+#ifndef _LAS_PARSER_H
+#define _LAS_PARSER_H
+
+#include "expr.h"
+#include "las.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    expr_t * parse_expr(pcode_t *);
+    instr_t * parse_instr(pcode_t *, const char *);
+    acode_t * parse(pcode_t *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LAS_PARSER_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
